fd73835eb4fc6bde938d465e3b37413e9a66bb0e
ClaireV38 <claireverdier00@gmail.com>
2020-09-30 15:41:32 +0200

    creation of mobile version of footer

---
+++ public/_footer.php
@@ -0,0 +1,24 @@
+<footer class="footer">
+    <link rel="stylesheet" href="assets/style/style.css"/>
+    <div class="logossocialnetworks">
+        <img
+                src="assets/img/facebooklogo.png"
+                alt="logo facebook"
+        />
+        <img
+                src="assets/img/twitterlogo.png"
+                alt="logo twitter"
+        />
+        <img
+                src="assets/img/instagramlogo.jpg"
+                alt="logo instagram"
+        />
+    </div
+    <div class="copyright">
+        @2020 Wildgamers
+    </div>
+</footer>
+
+
+
+
