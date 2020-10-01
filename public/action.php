d78925dec9c3e5649ae79cc133c95835c4086dcb
Yohan Tahir <yohantahir@gmail.com>
2020-10-01 22:48:29 +0200

    modif du action.php

--- public/action.php
+++ public/action.php
@@ -1,3 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/style/style.css" />
+    <link href='https://fonts.googleapis.com/css?family=Roboto Slab' rel='stylesheet'>
+    <title>Action - WildGaming</title>
+</head>
+<body>
+<header>
+    <!-- Code HEADER -->
+</header>
+
+<div class="carousel">
+    <!-- Code Carousel -->
+</div>
+
+<section class="blocGame">
+    <div id="firstGame">
+        <div id="coverFirstGame"></div>
+        <div id="titleFirstGame"></div>
+        <div id="iconFirstGame"></div>
+        <div id="textFirstGame"></div>
+        <div id="buyFirstGame"></div>
+    </div>
+
+    <div id="secondGame">
+        <div id="coverSecondGame"></div>
+        <div id="titleSecondGame"></div>
+        <div id="iconSecondGame"></div>
+        <div id="textSecondGame"></div>
+        <div id="buySecondGame"></div>
+    </div>
+    <div id="thirdGame">
+        <div id="coverThirdGame"></div>
+        <div id="titleThirdGame"></div>
+        <div id="iconThirdGame"></div>
+        <div id="textThirdGame"></div>
+        <div id="buyThirdGame"></div>
+    </div>
+    <div id="fourthGame">
+        <div id="coverFourthGame"></div>
+        <div id="titleFourthGame"></div>
+        <div id="iconFourthGame"></div>
+        <div id="textFourthGame"></div>
+        <div id="buyFourthGame"></div>
+    </div>
+    <div id="fifthGame">
+        <div id="coverFifthGame"></div>
+        <div id="titleFifthGame"></div>
+        <div id="iconFifthGame"></div>
+        <div id="textFifthGame"></div>
+        <div id="buyFifthGame"></div>
+    </div>
+    <div id="sixthGame">
+        <div id="coverSixthGame"></div>
+        <div id="titleSixthGame"></div>
+        <div id="iconSixthGame"></div>
+        <div id="textSixthGame"></div>
+        <div id="buySixthGame"></div>
+    </div>
+
+</section>
+
+<form>
+    <!-- Code pour le form -->
+</form>
+
 <?php
 
-include 'public/_footer.php';
+include '_footer.php';
+?>
+</body>
