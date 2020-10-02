37079f17fef6873ae0825479e2dfa8878651b195
Maxime Autechaud <maxime.autechaud@gmail.com>
2020-10-02 14:05:00 +0200

    allez stp marche

--- public/_headerSidePage.php
+++ public/_headerSidePage.php
@@ -9,13 +9,13 @@
 
     <nav>
         <ul>
-            <li><a href="index.php">homepage</a></li>
+            <li><a href="index.php">Home</a></li>
             <li><a href="RPG.php">RPG</a></li>
             <li><a href="FPS.php">FPS</a></li>
-            <li><a href="action.php">action/aventure</a></li>
-            <li><a href="strategie.php">stratégie</a></li>
-            <li><a href="sport.php">sport</a></li>
-            <li><a href="simulation.php">simulation</a></li>
+            <li><a href="action.php">Action/Aventure</a></li>
+            <li><a href="strategie.php">Stratégie</a></li>
+            <li><a href="sport.php">Sport</a></li>
+            <li><a href="simulation.php">Simulation</a></li>
         </ul>
     </nav>
 </header>
