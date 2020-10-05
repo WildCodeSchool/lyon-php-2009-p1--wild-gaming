8934e1e93cdf488d900c7807a0f4b92ed396287d
Rainbow-of-Dawn <as.linkpro@gmail.com>
2020-10-05 15:56:28 +0100

    added carousel, pics and form

--- public/script.js
+++ public/script.js
@@ -13,14 +13,14 @@ function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("gameSlides");
     var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 0}
-    if (n < 0) {slideIndex = slides.length}
+    if (n > slides.length-1) {slideIndex = 0}
+    if (n < 0) {slideIndex = slides.length-1}
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
+    slides[slideIndex].style.display = "block";
+    dots[slideIndex].className += " active";
 }
