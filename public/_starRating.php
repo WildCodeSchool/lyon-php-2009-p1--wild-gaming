329a810f25b4ef346133d0516eeaede8c23dd756
ClaireV38 <claireverdier00@gmail.com>
2020-10-05 21:17:35 +0200

    created a star rating for the form

--- public/_starRating.php
+++ public/_starRating.php
@@ -1,21 +1,11 @@
 <link rel="stylesheet" href="assets/style/style.css"/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<form method="POST">
-    <div class="already_played">
-        <label for="game_played"> A quel jeu de ce theme avez-vous joué ?</label>
-        <select name="game_played" id="game_played">
-            <option value="game1">nom du jeu 1</option>
-            <option value="game2">nom du jeu 2</option>
-            <option value="game3">nom du jeu 3</option>
-            <option value="game4">nom du jeu 4</option>
-            <option value="game5">nom du jeu 5</option>
-            <option value="game6">nom du jeu 6</option>
-        </select>
-    </div>
+<form>
     <div class="star_rating">
         <label for="Star_Rating"> notez ce jeu : </label>
-        <div class="lign_of_stars">
-            <input type="checkb
+        <div class="line_of_stars">
+            <input type="checkbox" id="star5" name="Star_Rating" class="star" value="5">
+            <label for="star5" class="star" title="5 stars"></label>
             <input type="checkbox" id="star4" name="Star_Rating" class="star" value="4">
             <label for="star4" class="star" title="4 stars"></label>
             <input type="checkbox" id="star3" name="Star_Rating" class="star" value="3">
@@ -25,9 +15,5 @@
             <input type="checkbox" id="star1" name="Star_Rating" class="star" value="1">
             <label for="star1" class="star" title="1 stars"></label>
         </div>
-    </div>
-    <div class="text_area">
-        <label for="msg">Message:</label>
-        <textarea id="msg" name="user_message"></textarea>
     </div>
 </form>
