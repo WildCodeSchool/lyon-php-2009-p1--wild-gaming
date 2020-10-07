55e2a48f6b0516ccb0e287df6d642887e3144621
ClaireV38 <claireverdier00@gmail.com>
2020-10-07 09:12:32 +0200

    created form template

--- public/arrayInForm.php
+++ public/arrayInForm.php
@@ -1,17 +1,54 @@
-<?php include_once '../src/function.php'; ?>
+<?php include_once '../src/function.php';
+
+$theme = "";
+$aliasTheme "";
+echo ($_SERVER['PHP_SELF']);
+switch ($_SERVER['PHP_SELF']){
+    case('/action.php'):
+        $theme = "Action/Aventure";
+        $aliasTheme = "action";
+        break;
+    case('/FPS.php'):
+        $theme = "FPS";
+        $aliasTheme = "FPS";
+        break;
+    case('/RPG.php'):
+        $theme = "RPG";
+        $aliasTheme = "RPG";
+        break;
+    case('/simulation.php'):
+        $theme = "simulation"
+        $aliasTheme = "simulation";
+        break;
+    case('/sport.php'):
+        $theme = "sport";
+        $aliasTheme = "sport";
+        break;
+    case('/strategie.php'):
+        $theme = "strategie";
+        $aliasTheme = "strategie";
+        break;
+    case('/arrayInForm.php'):
+        $theme = "FPS";
+        $aliasTheme = "FPS";
+        break;
+    default:
+        echo "error theme don't match";
+}
 
 
+        ?>
 
 <form action="">
-        <label for=" <?php $_SERVEUR['PHP_SELF'] ?>gamelist">What game did you play?</label>
-        <select id="<?php $_SERVEUR['PHP_SELF'] ?>gamelist" name="<?php $_SERVEUR['PHP_SELF'] ?>gamelist">
-            <?php foreach($dataBase["Action/Aventure"] as $game => $infos) { ?>
+        <label for="<?= $aliasTheme ?>gamelist">What game did you play?</label>
+        <select id="<?= $aliasTheme ?>gamelist" name="<?= $aliasTheme ?>gamelist">
+            <?php foreach($dataBase[$theme] as $game => $infos) { ?>
             <option value="<?= $game ?>"><?= $game ?></option>
             <?php } ?>
         </select>
 
-        <label for="<?php $_SERVEUR['PHP_SELF'] ?>rating">How would you rate it?</label>
-        <select id="<?php $_SERVEUR['PHP_SELF'] ?>rating" name="<?php $_SERVEUR['PHP_SELF'] ?>rating">
+        <label for="<?= $aliasTheme ?>rating">How would you rate it?</label>
+        <select id="<?= $aliasTheme ?>rating" name="<?= $aliasTheme ?>rating">
             <option value="excellent">Excellent!</option>
             <option value="good">Good</option>
             <option value="decent">Decent but not my favorite game</option>
